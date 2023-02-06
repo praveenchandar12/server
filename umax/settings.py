c0f5ce2456b2818fd69fea1cd862460dbaebf804
praveenchandar12 <38370163+praveenchandar12@users.noreply.github.com>
2023-02-06 15:00:08 +0530

    Update settings.py

--- umax/settings.py
+++ umax/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,9 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS=[path(BASE_DIR, 'static']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
