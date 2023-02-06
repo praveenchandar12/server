f0128d8f2bb59468d1b3230d77f2c607d46ee48b
praveenchandar12 <38370163+praveenchandar12@users.noreply.github.com>
2023-02-06 15:01:45 +0530

    Update settings.py

--- umax/settings.py
+++ umax/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[path(BASE_DIR, 'static']
+STATICFILES_DIRS =[BASE_DIR / 'static'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
